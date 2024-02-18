8a25c64fcbe01673f32675732d62205f2e3733ae
Nicolò Bartolini <nicolobartolinii@gmail.com>
2024-02-18 17:13:21 +0100

    Cannone rotante

--- Zibaldone/Assets/Script/CannoneScript.cs
+++ Zibaldone/Assets/Script/CannoneScript.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CannoneScript : MonoBehaviour
-{
+public class CannoneScript : MonoBehaviour {
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,21 @@ public class CannoneScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RotateCannonTowardsMouse();
+    }
+
+    void RotateCannonTowardsMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(
+                mousePosition.x - transform.position.x,
+                mousePosition.y - transform.position.y
+            );
+
+        float angle = Mathf.Atan2( direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, angle - 90);
     }
 }
